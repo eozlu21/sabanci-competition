3f2cb23626aabdd8540c6d4e30095a50904f64c9
Ege Erdem Özlü <eozlu21@ku.edu.tr>
2025-04-13 18:49:56 +0300

    Added part two

--- model_part_two.py
+++ model_part_two.py
@@ -10,39 +10,45 @@ def build_part_two_model(instance: HealthCenterInstancePartTwo) -> gp.Model:
     assignments = instance.assignments
     Q = 10_000  # Might be a parameter in the future
     N = instance.num_communities
-    M = instance.num_health_centers
+    M = instance.num_health_centers + 1  # Including depot
     P = {j: instance.nodes[j]["population"] for j in range(N)}
     Y = {
         (i, j): 1 if j in instance.assignments[i][1] else 0
         for i in range(M)
         for j in range(N)
     }
-
     T = {i: sum(P[j] * Y[i, j] for j in range(N)) for i in range(M)}
 
+    # Decision variables
     z = model.addVars(M, M, vtype=GRB.BINARY, name="z")
     u = model.addVars(M, vtype=GRB.CONTINUOUS, name="u")
 
+    # Objective: sum of distances for all arcs used (i != k)
     model.setObjective(
         gp.quicksum(
             z[i, k] * distances[assignments[i][0], assignments[k][0]]
-            for i in range(1, M)
-            for k in range(M)
-        )
+            for i in range(M)
+            for k in range(1, M)
+            if i != k
+        ),
+        GRB.MINIMIZE,
     )
 
-    # Constraints
-    model.addConstrs(gp.quicksum(z[i, k] for i in range(1, M)) == 1 for k in range(M))
+    model.addConstrs(gp.quicksum(z[i, k] for i in range(M)) == 1 for k in range(1, M))
+    model.addConstrs(
+        gp.quicksum(z[k, i] for i in range(1, M)) <= 1 for k in range(1, M)
+    )
 
-    model.addConstrs(gp.quicksum(z[i, k] for i in range(M)) <= 1 for k in range(M))
-
-    model.addConstr(u[0] == 0)
+    model.addConstr(u[0] == 0, name="u0_zero")
 
     model.addConstrs(
-        u[i] - u[k] + Q * z[i, k] <= Q - T[k]
-        for i in range(1, M)
-        for k in range(M)
-        if i != k
+        (
+            u[i] - u[k] + Q * z[i, k] <= Q - T[k]
+            for i in range(M)
+            for k in range(1, M)
+            if i != k
+        ),
+        name="mtz",
     )
 
     return model
